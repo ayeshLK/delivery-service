25e2f42008af99b4b275e1be11167479ef56a474
ayeshLK <ayesh9303@gmail.com>
2022-04-21 11:09:25 +0530

    Update dependency version management logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.6.6"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.6.10"
-	kotlin("plugin.spring") version "1.6.10"
+	id("org.springframework.boot")
+	id("io.spring.dependency-management")
+	kotlin("jvm")
+	kotlin("plugin.spring")
 }
 
-group = "io.gorillas"
-version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
@@ -16,12 +14,15 @@ repositories {
 }
 
 dependencies {
+	val graphqlKotlinVersion: String by project
+	val jacksonKotlinVersion: String by project
+
 	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("com.expediagroup:graphql-kotlin-spring-server:6.0.0-alpha.0")
+	implementation("com.expediagroup:graphql-kotlin-spring-server:$graphqlKotlinVersion")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.1")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonKotlinVersion")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,18 @@
 rootProject.name = "delivery-service"
+
+pluginManagement {
+    val properties = java.util.Properties()
+    properties.load(File(rootDir, "gradle.properties").inputStream())
+
+    val springBootVersion: String by properties
+    val springDependencyManagement: String by properties
+    val springPlugin: String by properties
+    val kotlinJvmVersion: String by properties
+
+    plugins {
+        id("org.springframework.boot") version springBootVersion
+        id("io.spring.dependency-management") version springDependencyManagement
+        kotlin("plugin.spring") version springPlugin
+        kotlin("jvm") version kotlinJvmVersion
+    }
+}
